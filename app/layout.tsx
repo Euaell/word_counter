f21a178c6c891d66eb56c7d194c4343b86799c24
Euaell <euaelmeko@gmail.com>
2024-10-14 00:25:47 +0300

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google"
 import { Poppins } from 'next/font/google';
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: Readonly<{children: React.React
 				className={`${poppins.className} antialiased`}
 			>
 				{children}
+				<GoogleAnalytics gaId="G-M7J30X3P4K" />
 			</body>
 		</html>
 	)
